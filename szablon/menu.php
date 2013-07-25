5816051e34b15773ddbc3f5022f2b416f019c9af
dglinka <dorota.glinka@op.pl>
2013-07-26 00:08:54 +0200

    wyglad

--- szablon/menu.php
+++ szablon/menu.php
@@ -1,10 +1,17 @@
 <div>
   <br>
-    <div id=menu>
-        <a id=a_menu href = "dodaj.php">Dodaj notatkę</a>
-    </div>
-    
-    <div id=menu>
-        <a id=a_menu href = "index.php">Twoje notatki</a>
-    </div>
+  <table id = menu>
+    <tr>
+      <td>
+          <div id=menu_dodaj>
+              <a id=a_menu href = "dodaj.php">Dodaj notatkę</a>
+          </div>
+      </td>
+      <td>    
+          <div id=menu_notatki>
+              <a id=a_menu href = "index.php">Twoje notatki</a>
+          </div>
+      </td>
+    </tr>
+  </table>  
 </div>
